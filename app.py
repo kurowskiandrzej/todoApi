29da993eca4846e8559b17ff8d7cdfbc50e391ef
Andrzej Kurowski <kurowskiandrzej@mail.com>
2022-03-17 20:59:46 +0100

    add User-Agent header processing and refactor

--- app.py
+++ app.py
@@ -11,7 +11,12 @@ app.register_blueprint(login_view)
 @app.before_request
 def before_request():
     g.locale = request.headers.get('Accept-Language')
+    g.user_agent = request.headers.get('User-Agent')
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(
+        debug=True,
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 8080))
+    )
