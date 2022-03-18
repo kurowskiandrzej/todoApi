cc57592879f66b1cb373e6308b9d0667e85cce45
kurowskiandrzej <53493158+kurowskiandrzej@users.noreply.github.com>
2022-03-18 09:50:24 +0100

    Update use_case_provider.py

--- dependency_injection/use_case_provider.py
+++ dependency_injection/use_case_provider.py
@@ -1,8 +1,10 @@
 from domain.use_case.use_case_wrapper.LoginViewModelUseCases import LoginViewModelUseCases
+from domain.use_case.use_case_wrapper.RegisterViewModelUseCases import RegisterViewModelUseCases
 from dependency_injection.repository_provider import repositories
 from domain.repository.ToDoRepository import ToDoRepository
 
 
 use_cases = {
-    LoginViewModelUseCases: LoginViewModelUseCases(repositories[ToDoRepository])
+    LoginViewModelUseCases: LoginViewModelUseCases(repositories[ToDoRepository]),
+    RegisterViewModelUseCases: RegisterViewModelUseCases(repositories[ToDoRepository])
 }
