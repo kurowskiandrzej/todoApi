2cf1fad73e541c10e91a799fd1880a3a33099343
kurowskiandrzej <53493158+kurowskiandrzej@users.noreply.github.com>
2022-03-18 09:49:22 +0100

    Update view_model_provider.py

--- dependency_injection/view_model_provider.py
+++ dependency_injection/view_model_provider.py
@@ -1,10 +1,12 @@
 from presentation.view_model.LoginViewModel import LoginViewModel
 from domain.use_case.use_case_wrapper.LoginViewModelUseCases import LoginViewModelUseCases
+from domain.use_case.use_case_wrapper.RegisterViewModelUseCases import RegisterViewModelUseCases
 from dependency_injection.use_case_provider import use_cases
 
 
 view_models = {
-    LoginViewModel: LoginViewModel(use_cases[LoginViewModelUseCases])
+    LoginViewModel: LoginViewModel(use_cases[LoginViewModelUseCases]),
+    RegisterViewModel: RegisterViewModel(use_cases[RegisterViewModelUseCases])
 }
 
 
