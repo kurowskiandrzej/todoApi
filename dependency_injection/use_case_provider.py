480cc0ea613b42cf14c87c960ee5b60f48b5a23e
Andrzej Kurowski <kurowskiandrzej@mail.com>
2022-03-21 18:51:50 +0100

    fix missing arguments

--- dependency_injection/use_case_provider.py
+++ dependency_injection/use_case_provider.py
@@ -1,6 +1,7 @@
 from domain.use_case.use_case_wrapper.login_view_model_use_cases import LoginViewModelUseCases
 from domain.use_case.use_case_wrapper.register_view_model_use_cases import RegisterViewModelUseCases
 from domain.use_case.login_user_use_case import LoginUserUseCase
+from domain.use_case.register_user_use_case import RegisterUserUseCase
 from domain.use_case.validate_email_use_case import ValidateEmailUseCase
 from domain.use_case.validate_password_use_case import ValidatePasswordUseCase
 from dependency_injection.repository_provider import repositories
@@ -18,5 +19,9 @@ use_cases = {
         ValidateEmailUseCase(),
         ValidatePasswordUseCase()
     ),
-    RegisterViewModelUseCases: RegisterViewModelUseCases(repositories[ToDoRepository])
+    RegisterViewModelUseCases: RegisterViewModelUseCases(
+        RegisterUserUseCase(repositories[ToDoRepository]),
+        ValidateEmailUseCase(),
+        ValidatePasswordUseCase()
+    )
 }
