25466b60096391fb0277e9bb113b21fd4f6a9332
Andrzej Kurowski <kurowskiandrzej@mail.com>
2022-03-28 00:20:00 +0200

    fix small bug

--- presentation/view_model/list_view_model.py
+++ presentation/view_model/list_view_model.py
@@ -9,7 +9,7 @@ class ListViewModel:
         return self.__use_case.decode_jwt(secret, token)
 
     def post_list(self, user_id: int, list_name: str):
-        self.__use_case.post_list(user_id, list_name)
+        return self.__use_case.post_list(user_id, list_name)
 
     def get_all_lists(self, user_id):
         return self.__use_case.get_all_lists(user_id)
