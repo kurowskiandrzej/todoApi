46a7ff36460c6d387af249f7f3a8527dd435e24b
Andrzej Kurowski <kurowskiandrzej@mail.com>
2022-04-04 22:00:59 +0200

    remove unknown error string resource

--- localization/resources/eng.py
+++ localization/resources/eng.py
@@ -1,6 +1,5 @@
 eng = {
     'no_such_user': 'No such user',
-    'unknown_error': 'Unknown error',
     'incorrect_password': 'Incorrect Password',
     'user_already_exists': 'User already exists',
     'incorrect_email_format': 'Incorrect e-mail format',
--- localization/resources/pl.py
+++ localization/resources/pl.py
@@ -1,6 +1,5 @@
 pl = {
     'no_such_user': 'Nie ma takiego użytkownika',
-    'unknown_error': 'Nieznany błąd',
     'incorrect_password': 'Nieprawidłowe hasło',
     'user_already_exists': 'Użytkownik już istnieje',
     'incorrect_email_format': 'Nieprawidłowy format adresu e-mail',
