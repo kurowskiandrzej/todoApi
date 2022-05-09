42398d645b6754c4dc9bdaf5b9bd037d4e4a4224
Andrzej Kurowski <kurowskiandrzej@mail.com>
2022-05-09 04:04:22 +0200

    allow all headers

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from presentation.view.task_view import task_view
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
-app.config['Access-Control-Allow-Headers'] = 'Content-Type'
+app.config['Access-Control-Allow-Headers'] = '*'
 
 app.register_blueprint(login_view)
 app.register_blueprint(register_view)
